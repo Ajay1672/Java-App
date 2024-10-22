89cda0d4fe6a5a75e1a16391e2df2bc132740395
Ajay1672 <mauryaajay9594@gmail.com>
2024-10-22 17:34:35 +0530

    V1 commit

---
+++ c/Console_Java_App/src/main/java/com/example/App/MainApplication.java
@@ -0,0 +1,15 @@
+package com.example.ajay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjayApplication.class, args);
+		
+		System.out.println("Welcome to Devops Institute");
+	}
+
+}
